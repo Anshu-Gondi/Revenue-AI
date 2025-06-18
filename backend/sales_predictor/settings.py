7d6c71f5fb3d6742296adadcc1af572537bd3d3b
Anshu-Gondi <agondi982@gmail.com>
2025-06-18 11:37:37 +0530

    Initial commit

--- backend/sales_predictor/settings.py
+++ backend/sales_predictor/settings.py
@@ -167,6 +167,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Required by WhiteNoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
